38eb27e09e309eb0472192a82f59e09410e9c5fa
Toby Smyth <tobs169@gmail.com>
2014-09-12 19:22:09 +0100

    scalable grid drawing

--- Main.hs
+++ Main.hs
@@ -2,8 +2,11 @@
 import Data.Monoid ((<>), mconcat)
 
 import Graphics.Gloss.Interface.IO.Game
+import Data.Tuple (swap)
+import Control.Applicative ((<$>))
+import Control.Applicative ((<*>))
 
-data Game = Game { grid :: Grid, window :: Window }
+data Game = Game { game_grid :: Grid, window :: Window }
 
 type Grid = [[Bool]]
 data Window = Window {game_size :: (Int, Int)}
@@ -37,33 +40,53 @@ handleInput  _  = return
     
 
 drawBoard :: Game -> IO Picture
-drawBoard board = return (filledSquares <> gridLines) 
+drawBoard board = return (filledSquares (squareWidth s) (game_grid board) <> gridLines s) 
     where
-      size = (fst . game_size . window) board
-      s = squareWidth size
-      drawLine = color red . line
-      gridLines = (mconcat . map  drawLine . getLinePoints) size                 
-      filledSquares = mconcat
-              [ translate (fromIntegral  (x - 1) * s)
-                        (fromIntegral  (y - 1) * s) $
-                
-                  color white (rectangleSolid s s)
-                 
+      s = (fst . game_size . window) board
+      
+                       
+      
+
+filledSquares :: Float -> Grid -> Picture
+filledSquares s grid = mconcat
+              [ translate (fromIntegral (x - 1) * s)
+                        (fromIntegral (y - 1) * s) $
+                        color white (rectangleSolid s s)
               | x <- [0..2]
               , y <- [0..2]
-              , True <- [(grid board !! x) !! y]
+              , True <- [(grid !! x) !! y]
               ]
                         
 
+gridLines :: Int -> Picture
+gridLines  = mconcat . map  drawLine . getLinePoints
+    where drawLine = color red . line
+
 getLinePoints :: Int -> [Path]
-getLinePoints size = [[(-l, -h), (-l, h)] ,
-                   [(l, -h), (l, h)] ,
-                   [(-h, l), (h, l)] ,
-                   [(-h, -l), (h, -l)]]
-    where 
-      h = fromIntegral (size `div` 2)
-      l =  fromIntegral (size `div`  6)
-           
+getLinePoints size = getGridPaths 6  s s
+                     where s = fromIntegral size
+
+intervals :: Int -> Float -> [Float]
+intervals n s = map ((*) $ s / n') [-n'' .. n'']  
+    where  n' = (fromIntegral n) 
+           n'' = n' / 2
+
+getGridPaths :: Int -> Float -> Float -> [Path]
+getGridPaths n w h = (++) vertCoords horCoords
+                     where vertCoords = map (verticalLine h) (intervals n w)
+                           horCoords = map (horizontalLine w) (intervals n h)
+                           
+
+
+verticalLine :: Float -> Float -> Path
+verticalLine l pos  = [(pos, -l'), (pos, l')]
+                     where l' =  l / 2
+
+horizontalLine :: Float -> Float -> Path
+horizontalLine l  = map swap .  verticalLine l
+
+
 squareWidth :: Int -> Float
 squareWidth size = (fromIntegral . div size) 3
       
+

2a89e26ecc7c732dd61c0d99f622a787551f1593
Toby Smyth <tobs169@gmail.com>
2014-09-13 15:13:41 +0100

    Scalable grid drawing

--- Main.hs
+++ Main.hs
@@ -18,7 +18,7 @@ initialWindow :: Window
 initialWindow = Window (500, 500)
 
 initialGrid :: Grid
-initialGrid = replicate 3 [True, False, True]
+initialGrid = replicate 4 [False, True, False, True]
 
 main :: IO()
 main = do
@@ -40,41 +40,60 @@ handleInput  _  = return
     
 
 drawBoard :: Game -> IO Picture
-drawBoard board = return (filledSquares (squareWidth s) (game_grid board) <> gridLines s) 
+drawBoard board = return (drawSquares s grid <> uncurry gridLines (gridSize grid) s) 
     where
       s = (fst . game_size . window) board
-      
+      grid = game_grid board
                        
-      
-
 filledSquares :: Float -> Grid -> Picture
 filledSquares s grid = mconcat
-              [ translate (fromIntegral (x - 1) * s)
-                        (fromIntegral (y - 1) * s) $
-                        color white (rectangleSolid s s)
-              | x <- [0..2]
-              , y <- [0..2]
+              [ drawSquareAt (translateCornerTo nx x) (translateCornerTo ny y) (sqWidth nx) (sqWidth ny)
+              | x <- [0 .. nx - 1]
+              , y <- [0.. ny - 1]
               , True <- [(grid !! x) !! y]
               ]
-                        
+    where (nx , ny) = gridSize grid
+          sqWidth n = s / fromIntegral  n
+          translateCornerTo n i = translateCoord s n ((fromIntegral i) + 0.5)
+         
 
-gridLines :: Int -> Picture
-gridLines  = mconcat . map  drawLine . getLinePoints
+
+          
+drawSquareAt :: Float -> Float -> Float -> Float  -> Picture
+drawSquareAt x y w h  =  translate x y $
+                        color white (rectangleSolid w h)
+    
+
+gridSize :: Grid -> (Int , Int)
+gridSize = (,) <$> length . head <*> length
+
+drawSquares :: Int -> Grid -> Picture
+drawSquares s grid = filledSquares (fromIntegral s) grid
+
+gridLines :: Int -> Int -> Int -> Picture
+gridLines nw nh  = drawLines . getLinePoints nw nh
+
+drawLines :: [Path] -> Picture
+drawLines =  mconcat . map  drawLine
     where drawLine = color red . line
 
-getLinePoints :: Int -> [Path]
-getLinePoints size = getGridPaths 6  s s
+getLinePoints :: Int -> Int -> Int -> [Path]
+getLinePoints nw nh size = getGridPaths nw nh  s s
                      where s = fromIntegral size
 
 intervals :: Int -> Float -> [Float]
-intervals n s = map ((*) $ s / n') [-n'' .. n'']  
-    where  n' = (fromIntegral n) 
-           n'' = n' / 2
+intervals n s = map ((translateCoord s n) . fromIntegral) [0 .. n]
 
-getGridPaths :: Int -> Float -> Float -> [Path]
-getGridPaths n w h = (++) vertCoords horCoords
-                     where vertCoords = map (verticalLine h) (intervals n w)
-                           horCoords = map (horizontalLine w) (intervals n h)
+translateCoord :: Float -> Int -> Float -> Float
+translateCoord s n i = s * (i / n')  - (s / 2)
+    where  n' = fromIntegral n 
+
+getGridPaths :: Int -> Int -> Float -> Float -> [Path]
+getGridPaths nw nh  w h = (++) vertCoords horCoords
+                     where
+                       f g n s = map g $ intervals n s
+                       vertCoords = f (verticalLine h) nw w
+                       horCoords = f (horizontalLine w) nh h
                            
 
 
@@ -84,9 +103,5 @@ verticalLine l pos  = [(pos, -l'), (pos, l')]
 
 horizontalLine :: Float -> Float -> Path
 horizontalLine l  = map swap .  verticalLine l
-
-
-squareWidth :: Int -> Float
-squareWidth size = (fromIntegral . div size) 3
       
 
